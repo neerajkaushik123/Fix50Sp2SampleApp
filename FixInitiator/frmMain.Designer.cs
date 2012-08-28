5900d186500f1e8bf86d061e1fea5a06d4633455
neerajkaushik123 <neeraj.kaushik@live.in>
2012-08-28 18:04:39 +0530

    frmmain added

---
+++ FixInitiator/frmMain.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace FixInitiator
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnStartMarketPrice = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(20, 12);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "&Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Enabled = false;
+            this.btnDisconnect.Location = new System.Drawing.Point(101, 12);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(75, 23);
+            this.btnDisconnect.TabIndex = 1;
+            this.btnDisconnect.Text = "&Disconnect";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(20, 69);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(471, 290);
+            this.listBox1.TabIndex = 7;
+            // 
+            // btnStartMarketPrice
+            // 
+            this.btnStartMarketPrice.Enabled = false;
+            this.btnStartMarketPrice.Location = new System.Drawing.Point(182, 12);
+            this.btnStartMarketPrice.Name = "btnStartMarketPrice";
+            this.btnStartMarketPrice.Size = new System.Drawing.Size(121, 25);
+            this.btnStartMarketPrice.TabIndex = 11;
+            this.btnStartMarketPrice.Text = "&Start Marketprice";
+            this.btnStartMarketPrice.UseVisualStyleBackColor = true;
+            this.btnStartMarketPrice.Click += new System.EventHandler(this.btnStartMarketPrice_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(318, 12);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(140, 25);
+            this.btnExit.TabIndex = 12;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(541, 393);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnStartMarketPrice);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnDisconnect);
+            this.Controls.Add(this.btnConnect);
+            this.Name = "frmMain";
+            this.Text = "Market Watch";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnStartMarketPrice;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ FixInitiator/frmMain.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using QuickFix;
+using QuickFix.Transport;
+using Message = QuickFix.Message;
+
+namespace FixInitiator
+{
+    public partial class frmMain : Form
+    {
+        private FixClient50Sp2 _client;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+           
+            // FIX app settings and related
+            var settings = new SessionSettings("C:\\initiator.cfg");
+
+            // FIX application setup
+            MessageStoreFactory storeFactory = new FileStoreFactory(settings);
+            LogFactory logFactory = new FileLogFactory(settings);
+            _client = new FixClient50Sp2(settings);
+
+            IInitiator initiator = new SocketInitiator(_client, storeFactory, settings, logFactory);
+            _client.Initiator = initiator;
+
+            _client.OnProgress += _client_OnProgress;
+            _client.LogonEvent += ClientLogonEvent;
+            _client.MessageEvent += ClientMessageEvent;
+            _client.LogoutEvent += ClientLogoutEvent;
+            _client.OnMarketDataIncrementalRefresh += Client_OnMarketDataIncrementalRefresh;
+            
+        }
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            _client.Stop();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            //_client.onst
+            _client.Start();
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            if (_client != null && _client.Initiator.IsLoggedOn())
+                _client.Initiator.Stop();
+
+            base.OnClosing(e);
+        }
+
+        private void _client_OnProgress(string msg)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new Action<string>(_client_OnProgress), msg);
+                return;
+            }
+
+            AddItem(msg);
+        }
+
+      
+        private void ClientLogoutEvent()
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new Action(ClientLogoutEvent), null);
+                return;
+            }
+            AddItem("Log out called");
+            enableControls(false);
+        }
+
+        private void AddItem(string message)
+        {
+            if (listBox1.Items.Count > 50)
+                listBox1.Items.Clear();
+
+            listBox1.Items.Add(message);
+        }
+
+        private void ClientLogonEvent()
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new Action(ClientLogonEvent), null);
+                return;
+            }
+
+            AddItem("Logged on");
+
+            enableControls(true);
+        }
+
+        private void enableControls(bool enable)
+        {
+            btnDisconnect.Enabled = enable;
+            btnStartMarketPrice.Enabled = enable;
+            // btnUpdateSecurities.Enabled = enable;
+            btnConnect.Enabled = !enable;
+        }
+
+        private void ClientMessageEvent(Message arg1, bool arg2)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new Action<Message, bool>(ClientMessageEvent), arg1, arg2);
+                return;
+            }
+            AddItem(arg1.ToString());
+        }
+
+
+        private void Client_OnMarketDataIncrementalRefresh(MarketPrice obj)
+        {
+            AddItem(obj.ToString());
+        }
+
+        private void LogError(string msg)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new Action<string>(LogError), msg);
+                return;
+            }
+            AddItem(msg);
+        }
+
+        private void Client_OnMarketDataSnapshotFullRefresh(MarketPrice obj)
+        {
+            AddItem(obj.ToString());
+        }
+
+
+        private void btnStartMarketPrice_Click(object sender, EventArgs e)
+        {
+            if (btnStartMarketPrice.Text == "&Start Marketprice")
+            {
+                //TODO: this is sample symbol id, it can be replaced by real id
+                string sym = "MSFT";
+                _client.Subscribe(sym, _client.ActiveSessionId);
+
+                btnStartMarketPrice.Text = "&Stop";
+            }
+            else
+            {
+                btnStartMarketPrice.Text = "&Start Marketprice";
+
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
